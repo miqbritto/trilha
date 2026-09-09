d2a9df1e680afe8b2bab20adafa6bb26cb72f2aa
M. Britto <miqueias.silva@alghorit.com.br>
2026-09-09 15:13:38 -0300

    feat: refactor player deck

--- frontend/src/app/features/game/game.ts
+++ frontend/src/app/features/game/game.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Shell } from '../../shared/components/shell/shell';
 
 @Component({
@@ -8,5 +8,17 @@ import { Shell } from '../../shared/components/shell/shell';
   styleUrl: './game.scss',
 })
 export class Game {
+   readonly bars = Array.from({ length: 6 }, (_, i) => i)
+   readonly waves = Array.from({ length: 12 }, (_, i) => i)
+
+   protected readonly isPressed = signal(false);
+
+   pressButton() {
+      this.isPressed.set(true);
+
+      setTimeout(() => {
+         this.isPressed.set(false);
+      }, 1000);
+   }
 
 }
