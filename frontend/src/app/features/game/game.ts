91f17d5dcd69bf0629d7114ac526f3cd31f03217
M. Britto <miqueias.silva@alghorit.com.br>
2026-09-10 16:33:32 -0300

    feat: implement search filter

--- frontend/src/app/features/game/game.ts
+++ frontend/src/app/features/game/game.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { Shell } from '../../shared/components/shell/shell';
 import { MovieService } from '../../core/services/movie.service';
 import { Movie } from '../../core/models/movie';
+import { catchError, debounceTime, distinctUntilChanged, of, Subject, switchMap, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -11,13 +13,38 @@ import { Movie } from '../../core/models/movie';
   styleUrl: './game.scss',
 })
 export class Game {
-  private readonly movieService = inject(MovieService);
+
+   private readonly searchTerms$ = new Subject<string>();
+
+   private readonly movieService = inject(MovieService);
    readonly bars = Array.from({ length: 6 }, (_, i) => i)
    readonly waves = Array.from({ length: 12 }, (_, i) => i)
 
    protected readonly isPressed = signal(false);
    readonly suggestions = signal<Movie[] | null >(null);
    readonly selectedMovie = signal<Movie | null>(null)
+
+   constructor() {
+      this.searchTerms$.pipe(
+         debounceTime(200),
+         distinctUntilChanged(),
+         switchMap( search => {
+            if (search.length < 2) {
+               return of<Movie[]>([]);
+            }
+
+            return this.movieService.searchMovie(search).pipe(
+               catchError(error => {
+                  console.error("Erro ao buscar filmes: ", error)
+                  return of<Movie[]>([])
+               })
+            )
+         }),
+         takeUntilDestroyed()
+      ).subscribe(movies => {
+         this.suggestions.set(movies)
+      })
+   }
 
    pressButton() {
       this.isPressed.set(true);
@@ -28,9 +55,9 @@ export class Game {
    }
 
    searchMovie(search: string) {
-    this.selectedMovie.set(null);
-    this.suggestions.set([])
-    this.movieService.searchMovie(search);
+      this.selectedMovie.set(null);
+      this.suggestions.set([])
+      this.searchTerms$.next(search.trim());
    }
 
    selectMovie(movie: Movie) {
--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
   const port = process.env['PORT'] || 4000;
+  
   app.listen(port, (error) => {
     if (error) {
       throw error;
